9c438b8b7b898d59dfc03add107065221fea2f24
jdoorduin <55236230+jdoorduin@users.noreply.github.com>
2024-09-09 14:40:31 +0000

    bmi

--- static/js/myscript.js
+++ static/js/myscript.js
@@ -1,47 +1,48 @@
-var count = document.getElementById('count');
-var input = document.getElementById('input');
-var globalWordCounter = 0;
-var WORD_LIMIT = 100000;
+window.onload = () => {
+  let button = document.querySelector("#btn");
 
-input.addEventListener('keydown', function(e) {
-  if (globalWordCounter > WORD_LIMIT && e.code !== "Backspace") {
-    e.preventDefault();
-    return alert("You have reached the word limit");
-  }
-});
+  // Function for calculating BMI
+  button.addEventListener("click", calculateBMI);
+};
 
-input.addEventListener('keyup', function(e) {
-  wordCounter(e.target.value);
-});
+function calculateBMI() {
 
-function isWord(str) {
-  var alphaNumericFound = false;
-  for (var i = 0; i < str.length; i++) {
-    var code = str.charCodeAt(i);
-    if ((code > 47 && code < 58) || // numeric (0-9)
-        (code > 64 && code < 91) || // upper alpha (A-Z)
-        (code > 96 && code < 123)) { // lower alpha (a-z)
-      alphaNumericFound = true;
-      return alphaNumericFound;
-    }
-  }
-  return alphaNumericFound;
-}
+  /* Getting input from user into height variable.
+  Input is string so typecasting is necessary. */
+  let height = parseInt(document
+      .querySelector("#height").value);
 
-function wordCounter(text) {
-  var text = input.value.replace(/\n/g, " ");
-  var charCount = 0;
-  for (var i = 0; i < text.length; i++) {
-    charCount++;
+  /* Getting input from user into weight variable. 
+  Input is string so typecasting is necessary.*/
+  let weight = parseInt(document
+      .querySelector("#weight").value);
+
+  let result = document.querySelector("#result");
+
+  // Checking the user providing a proper
+  // value or not
+  if (height === "" || isNaN(height))
+      result.innerHTML = "Provide a valid Height!";
+
+  else if (weight === "" || isNaN(weight))
+      result.innerHTML = "Provide a valid Weight!";
+
+  // If both input is valid, calculate the bmi
+  else {
+
+      // Fixing upto 2 decimal places
+      let bmi = (weight / ((height * height)
+          / 10000)).toFixed(2);
+
+      // Dividing as per the bmi conditions
+      if (bmi < 18.6) result.innerHTML =
+          `Under Weight : <span>${bmi}</span>`;
+
+      else if (bmi >= 18.6 && bmi < 24.9)
+          result.innerHTML =
+              `Normal : <span>${bmi}</span>`;
+
+      else result.innerHTML =
+          `Over Weight : <span>${bmi}</span>`;
   }
-  text = text.split(' ');
-  var wordCount = 0;
-  for (var i = 0; i < text.length; i++) {
-    if (!text[i] == ' ' && isWord(text[i])) {
-      wordCount++;
-    }
-  }
-  var wordcharCount = wordCount.toString() + " Character Count: " + charCount.toString();
-  globalWordCounter = wordcharCount;
-  count.innerText = wordcharCount;
 }
